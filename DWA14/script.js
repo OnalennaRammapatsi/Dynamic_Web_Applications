73a05ea314d33390a588b71c3839cf70d46c85b4
OnalennaRammapatsi <othandekamotlanye@gmail.com>
2023-07-06 09:03:49 +0200

    DWA14

--- DWA14/script.js
+++ DWA14/script.js
@@ -1,170 +1,150 @@
-import { html, css, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
-const MAX_NUMBER = 20;
-const MIN_NUMBER = -10;
-const STEP_AMOUNT = 1;
+import {LitElement, css, html} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+const STEP_AMOUNT = 5;
 
 export class CounterComponent extends LitElement {
   static styles = css`
-  :root {
-    --color-dark-blue:#152238 ;
-    --color-white:#ffffff ;
-    --color-light-blue: #91BAD6 ;
-    --color-medium-blue:#23395d ;
-}
-* {
-    box-sizing: border-box;
-}
-html {
-    height: 100vh;
-}
-body {
-    margin: 0;
-    background: var(--color-medium-blue);
-    color: var(--color-white);
-    font-family: Roboto, Arial, Helvetica, sans-serif;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100%
-}
-.header {
-    text-align: center;
-}
-.controls {
-    background: yellowgreen;
-}
-.counter {
-    background: var(--color-dark-blue);
-}
-.counter_value {
-    width: 100%;
-    height: 15rem;
-    text-align: center;
-    font-size: 6rem;
-    font-weight: 900;
-    color: var(--color-white);
-    background: none;
-    border-width: 0;
-    border-bottom: 1px solid var(--color-light-blue);
-}
-.counter_actions {
-    display: flex;
-}
-.counter_button {
-    background: none;
-    width: 50%;
-    border-width: 0;
-    color: var(--color-white);
-    font-size: 3rem;
-    height: 10rem;
-    border-bottom: 1px solid var(--color-light-blue);
-    transition: transform 0.3s;
-    transform: translateY(0);
-}
-.counter_button:disabled {
-    opacity: 0.2
-}
-.counter_button:active {
-    background-color: var(--color-medium-blue );
-    transform: translateY(2%);
-}
-.counter_button_first {
-    border-right: 1px solid var(--color-light-blue)
-}
-.counter_button_second{
-    border-right: 1px solid var(--color-light-blue)
-}
-.header_title {
-    font-size: 4rem;
-    font-weight: 900;
-    color: var(--color-light-blue) ;
-}
-.footer {
-    background: var(--color-dark-blue);
-    color: var(--color-light-blue );
-    padding: 2rem;
-    font-size: 0.8rem;
-    text-align: center;
-}
-.footer_link {
-    color: var(--color-white);
-}
+    :host {
+      --color-green: #F49380;
+      --color-white: #fff;
+      --color-dark-grey: #333d;
+      --color-medium-grey: #424250;
+      --color-light--grey: #8D97A7;
+    }
+    * {
+      box-sizing: border-box;
+    }
+    html {
+      height: 100vh;
+    }
+    body {
+      margin: 0;
+      background: var(--color-medium-grey);
+      color: var(--color-white);
+      font-family: Roboto, Arial, Helvetica, sans-serif;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      height: 100%;
+    }
+    /* header */
+    .header {
+      text-align: center;
+    }
+    .header__title {
+      font-size: 3rem;
+      font-weight: 900;
+      color: var(--color-light--grey);
+    }
+    /* controls */
+    .controls {
+      background: black;
+    }
+    /* counter */
+    .counter {
+      background: var(--color-dark-grey);
+    }
+    .counter__value {
+      width: 100%;
+      height: 15rem;
+      text-align: center;
+      font-size: 6rem;
+      font-weight: 900;
+      background: none;
+      color: var(--color-white);
+      border-width: 0;
+      border-bottom: 1px solid var(--color-light--grey);
+    }
+    .counter__actions {
+      display: flex;
+    }
+    .counter__button {
+      background: none;
+      width: 50%;
+      border: 0;
+      color: var(--color-white);
+      font-size: 3rem;
+      height: 10rem;
+      border-bottom: 1px solid var(--color-light--grey);
+      transition: transform 0.3s;
+      transform: translateY(0);
+    }
+    .counter__button:disabled {
+      opacity: 0.2;
+    }
+    .counter__button:active {
+      background: var(--color-medium-grey);
+      transform: translateY(2%);
+    }
+    .counter__button_first {
+      border-right: 1px solid var(--color-light--grey);
+    }
+    .footer {
+      background: var(--color-dark-grey);
+      color: var(--color-light--grey);
+      padding: 2rem;
+      font-size: 0.8rem;
+      text-align: center;
+    }
+    .footer__link {
+      color: var(--color-white);
+    }
   `;
-
   static properties = {
     count: { type: Number },
-  
   };
-
   constructor() {
     super();
     this.count = 0;
-   
   }
-
-  increment() {
-    const newValue = this.count + STEP_AMOUNT;
-    if (newValue <= MAX_NUMBER) {
-      this.count = newValue;
-      this.disabled = false;
-    } else {
-      this.disabled = true;
-    }
+  subtractHandler() {
+    this.count -= STEP_AMOUNT;
+    this.requestUpdate();
   }
-
-  decrement() {
-    const newValue = this.count - STEP_AMOUNT;
-    if (newValue >= MIN_NUMBER) {
-      this.count = newValue;
-      this.disabled = false;
-    } else {
-      this.disabled = true;
-    }
+  addHandler() {
+    this.count += STEP_AMOUNT;
+    this.requestUpdate();
   }
-
-  reset() {
-    this.count = 0;
-    this.disabled = true;
-    this.showConfirmationMessage('The counter has been reset.');
-  }
-
-  showConfirmationMessage(message) {
-    alert(message);
-  }
-
   render() {
     return html`
-    <header class="header">
-    <h1 class="header_title" >Tally Count Application</h1>
-</header>
-<main class="counter">
-    <input class="counter_value" 
-    data-key="number" 
-    readonly value="0">
-    .value=${this.count}disabled
-
-    
-    <div class="counter_actions">
-        <sl-button data-key="subtract" class="counter_button counter_button_first">-</sl-button>
-        <sl-button data-key="add" class="counter_button counter_button_second" >+</sl-button>
-        <sl-button data-key="reset" class="counter_button">reset</sl-button>
-    </div>
-</main>
-<footer class="footer">
-</footer>
+      <div class="header">
+        <h1 class="header__title">Tally Count App</h1>
+      </div>
+      <div class="controls">
+        <div class="counter">
+          <input
+            class="counter__value"
+            type="number"
+            .value=${this.count}
+            disabled
+          />
+          <div class="counter__actions">
+            <button
+              class="counter__button counter__button_first"
+              @click=${this.subtractHandler}
+              ?disabled=${this.count <= MIN_NUMBER}
+            >
+              Subtract
+            </button>
+            <button
+              class="counter__button"
+              @click=${this.addHandler}
+              ?disabled=${this.count >= MAX_NUMBER}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="footer">
+      </div>
     `;
   }
 }
 
 
-
 customElements.define('counter-component', CounterComponent);
-
-
-
-
-
-
 
 
 /* const MAX_NUMBER = 20;
